eb4d9f3029278fd57241bfbceba16556d60f73b1
Andres Medrano <amedra13@gmail.com>
2020-10-19 23:22:49 -0700

    fix bubble effect and placehold other sections

--- src/App.js
+++ src/App.js
@@ -1,13 +1,37 @@
 import React from 'react';
 import Navbar from './Navbar';
 import BubblesEffect from './BubblesEffect';
+import FacebookIcon from '@material-ui/icons/Facebook';
+import LinkedInIcon from '@material-ui/icons/LinkedIn';
+import TwitterIcon from '@material-ui/icons/Twitter';
 import './App.css';
 
 function App() {
 	return (
 		<div className="app">
-			<BubblesEffect />
-			<Navbar />
+			<div className="app__main">
+				<Navbar />
+				<div className="app__info">
+					<div className="app__title">
+						<p>Welcome to my Portfolio</p>
+						<p>
+							I am an aspiriing web developer with an interest in all things
+							React.js
+						</p>
+					</div>
+					<div className="app__name">
+						<p>Andres Medrano</p>
+					</div>
+					<div className="app__socialMedia">
+						<FacebookIcon />
+						<LinkedInIcon />
+						<TwitterIcon />
+					</div>
+				</div>
+				<BubblesEffect />
+			</div>
+			<div className="app__aboutMe"></div>
+			<div className="app__portfolio"></div>
 		</div>
 	);
 }
